1d77bd998ea3c3909e119ad04e61994e79d770b3
gustianaeko <geko477@gmail.com>
2024-08-14 13:26:13 +0700

    2 a 2ab , nomer 2 done

---
+++ 2.js
@@ -0,0 +1,33 @@
+const hitungVoucher = (voucherDiskon, uangBelanja) => {
+  let biayaHarusDibayar = uangBelanja;
+  let kembalian = 0;
+  if (voucherDiskon === "DumbWaysJos") {
+    const diskon = 20_000;
+    if (uangBelanja >= 50_000) {
+      biayaHarusDibayar = biayaHarusDibayar - diskon;
+      kembalian = uangBelanja - biayaHarusDibayar;
+    }
+
+    console.log(`======================================
+    Uang yang harus dibayar : ${biayaHarusDibayar}
+    Diskon : ${diskon}
+    Kembalian : ${kembalian}
+======================================
+    `);
+  } else if (voucherDiskon === "DumbWaysMantap") {
+    const diskon = 40_000;
+    if (uangBelanja >= 80_000) {
+      biayaHarusDibayar = biayaHarusDibayar - diskon;
+      kembalian = uangBelanja - biayaHarusDibayar;
+    }
+
+    console.log(`======================================
+    Uang yang harus dibayar : ${biayaHarusDibayar}
+    Diskon : ${diskon}
+    Kembalian : ${kembalian}
+======================================
+    `);
+  }
+};
+
+hitungVoucher("DumbWaysJos", 100_000);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FinalTask-Stage-1
+
 This is my stage 1 final task
+
+1. Kesulitan : menetukan kodingan untuk mengecek angkat tersebut bilangan prima atau bukan
