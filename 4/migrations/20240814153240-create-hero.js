76fafa13ae04b8b2ab0dcaa49ee4250dd1b9f739
gustianaeko <geko477@gmail.com>
2024-08-14 23:17:01 +0700

    db init and migrate

---
+++ 4/migrations/20240814153240-create-hero.js
@@ -0,0 +1,49 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Heros", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      photo: {
+        type: Sequelize.STRING,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Users",
+          },
+          key: "id",
+        },
+      },
+      TypeId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Types",
+          },
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Heros");
+  },
+};
