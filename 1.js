e53f591a9d07a231347ac9ee0dc2ac600ad4eb2a
gustianaeko <geko477@gmail.com>
2024-08-14 13:00:57 +0700

    1a 1b done

--- 1.js
+++ 1.js
@@ -1,18 +1,36 @@
 const drawSikuSiku = (alasOrTinggi) => {
   if (alasOrTinggi < 1 || alasOrTinggi > 9) {
     console.log(
-      `Alas atau tinggi harus lebih dari 0 dan atau kurang dari 10 (0 < alas < 10)`
+      `Alas atau tinggi harus lebih dari 0 dan atau kurang dari 10 (0 < alas/tinggi < 10)`
     );
     return;
   }
 
-  let count = 0;
   let number = 2;
 
   for (let i = 1; i <= alasOrTinggi; i++) {
     let temp = "";
     for (let j = 0; j < i; j++) {
-      temp += "* ";
+      temp += "";
+
+      while (true) {
+        let isPrime = true;
+
+        for (let k = 2; k < number; k++) {
+          if (number % k === 0) {
+            isPrime = false;
+            break;
+          }
+        }
+
+        if (isPrime) {
+          temp += `${number} `;
+          number++;
+          break;
+        }
+
+        number++;
+      }
     }
     console.log(temp);
   }
