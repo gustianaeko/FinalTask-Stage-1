e6dab264103cc3647e2a180550a8a70ec74151a7
gustianaeko <geko477@gmail.com>
2024-08-14 12:15:06 +0700

    1b sketsa segitiga terbalik dengan bintang sudah tergambar, berikutnya bintang akan diganti dengan angka prima berlanjut

--- 1.js
+++ 1.js
@@ -1,10 +1,21 @@
 const drawSikuSiku = (alasOrTinggi) => {
   if (alasOrTinggi < 1 || alasOrTinggi > 9) {
-    console.log(`Alas atau tinggi harus lebih dari 0 dan atau kurang dari 10`);
+    console.log(
+      `Alas atau tinggi harus lebih dari 0 dan atau kurang dari 10 (0 < alas < 10)`
+    );
     return;
   }
 
-  console.log("Alas atau tinggi sesuai rules");
+  let count = 0;
+  let number = 2;
+
+  for (let i = 1; i <= alasOrTinggi; i++) {
+    let temp = "";
+    for (let j = 0; j < i; j++) {
+      temp += "* ";
+    }
+    console.log(temp);
+  }
 };
 
-drawSikuSiku(0);
+drawSikuSiku(7);
